52321da4c7e04cb82a0c46852f3a6d81733579c1
死老百姓 <96654161+Trinity-SYT-SECURITY@users.noreply.github.com>
2024-02-18 10:55:26 +0800

    Delete Python_class directory

--- Python_class/forFiles/寫入檔案.py
+++
@@ -1,8 +0,0 @@
-﻿# 簡易寫入檔案範例
-content = "meow"
-# 開啟檔案
-# 開啟 file1.txt檔案，並開啟[寫入]模式
-f = open("C:\\Users\\user\\Desktop\\coding-X\\Python_class\\forFiles\\file1.txt", "w")
-f.write(content)  # 寫入content的內容到檔案中
-f.close()  # 關閉檔案
-print("file1.txt檔案處理完成")  # 告訴使用者工作完成
